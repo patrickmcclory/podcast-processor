234aea6c84bf910a476c0b20862dd40c2f03418b
Patrick McClory <patrick@dualspark.com>
2015-05-24 18:59:51 +0200

    committing initial rev of vagrant setup and draft dockerfile

---
+++ processor/normalize.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 infile outfile"
+  echo "       $0 infile outfile noisefile"
+  exit 1
+fi
+
+SOX_CMD="sox $1 $2"
+
+if [ $# -eq 3 ]; then
+  sox "$3" -noiseprof noise.prof
+  SOX_CMD="$SOX_CMD noisered noise.prof 0.21"
+fi
+
+SOX_CMD="$SOX_CMD highpass 100"
+SOX_CMD="$SOX_CMD norm"
+SOX_CMD="$SOX_CMD compand 0.05,0.2 6:-54,-90,-36,-36,-24,-24,0,-12 0 -90 0.1"
+SOX_CMD="$SOX_CMD vad -T 0.6 -p 0.2 -t 5"
+SOX_CMD="$SOX_CMD fade 0.1"
+SOX_CMD="$SOX_CMD reverse"
+SOX_CMD="$SOX_CMD vad -T 0.6 -p 0.2 -t 5"
+SOX_CMD="$SOX_CMD fade 0.1"
+SOX_CMD="$SOX_CMD reverse"
+SOX_CMD="$SOX_CMD norm -0.5"
+
+eval "$SOX_CMD"
+
+if [ $# -eq 3 ]; then
+  rm -rf noise.prof
+fi
---
+++ processor/processaudio.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 infile "
+  echo "       $0 infile noisefile"
+  exit 1
+fi
+
+AUDIO_FILE_NAME=standalone_audio.mp3
+PROCESSED_FILE_NAME=processed_audio.mp3
+
+# strip audio
+
+ffmpeg -i $1 $AUDIO_FILE_NAME
+
+sox_cmd="normalize $AUDIO_FILE_NAME $PROCESSED_FILE_NAME"
+
+if [ $# -eq 2 ]; then
+sox_cmd="$sox_cmd $2"
+fi
+
+eval $sox_cmd
